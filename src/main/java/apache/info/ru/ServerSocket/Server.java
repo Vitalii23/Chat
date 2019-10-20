305c17f4275d02044e4575281839aebc3a61f87f
Vitalii <danilov.vit.an@gmai.com>
2019-10-20 18:19:30 +0700

    prototype chat

--- src/main/java/apache/info/ru/ServerSocket/Server.java
+++ src/main/java/apache/info/ru/ServerSocket/Server.java
@@ -1,23 +1,21 @@
 package apache.info.ru.ServerSocket;
 
-import apache.info.ru.DAO.ArchiveDAOImpl;
+import apache.info.ru.DataBase.JDBC;
 import apache.info.ru.Worker.Worker;
-import org.springframework.integration.jdbc.store.JdbcChannelMessageStore;
 
 public class Server {
     public void runServer(String port) {
         Worker workerServer = new Worker(port);
-       // JdbcChannelMessageStore jdbcChannelMessageStore = new JdbcChannelMessageStore();
-        ArchiveDAOImpl dao = new ArchiveDAOImpl();
         System.out.println("Started ServerSocket with " + " on " + port + "\n");
         while (true) {
             Worker worker = new Worker(workerServer);
+            JDBC jdbc = new JDBC();
             System.out.println("ClientSocket accepted");
             new Thread(() -> {
                 String line = worker.readLine();
                 System.out.println("Accepted: " + line + "\n");
-              // jdbcChannelMessageStore.addMessageToGroup(dao.saveMessageOrUpdate(line);)
                 worker.writeLine(line);
+                jdbc.insert(line);
                 worker.close();
             }).start();
         }
